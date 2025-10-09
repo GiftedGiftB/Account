70564db3081e1170b7f1d7bc5e108b582aaacde9
GiftedGiftB <asekhamegift@gmail.com>
2025-10-09 20:06:45 +0100

    class exercise

---
+++ src/CustomModifier.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CustomModifier {
+    public static void main(String[] args){
+        try {
+            // 1. create path that points to file
+            String filePath = "c:\\Users\\DELL\\Desktop\\Account.java";
+
+            //2. read file data using class Files
+            byte[] data = readDataFrom(filePath);
+            System.out.println(new String(data));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static byte[] readDataFrom(String filePath) throws IOException{
+        Path path = Paths.get(filePath);
+        byte[] data = Files.readAllBytes(path);
+        return data;
+    }
+}
